c5f0a85e437240df3b887da47634d22bb9b44df2
Rachel Kerner & Chris McMenomy <pair+bb8bear.caristopmer@devbootcamp.com>
2017-07-30 15:18:56 -0500

    Home page layout complete with title, text, and three buttons.

---
+++ src/pages/form-text/form-text.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FormTextPage } from './form-text';
+
+@NgModule({
+  declarations: [
+    FormTextPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FormTextPage),
+  ],
+})
+export class FormTextPageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http';
+//import { Http, RequestOptions, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-// import { ResponsePage } from '../response/response';
-import 'rxjs/add/operator/map';
+//import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+//import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -11,29 +10,29 @@ import 'rxjs/add/operator/map';
 })
 
 export class HomePage {
-  private emoteForm: FormGroup;
+  //private emoteForm: FormGroup;
 
-  constructor(public http: Http, public navCtrl: NavController, private formBuilder: FormBuilder) {
-    this.emoteForm = this.formBuilder.group({
-      emote: ['', Validators.required]
-    });
+  constructor(public navCtrl: NavController) {
+    // this.emoteForm = this.formBuilder.group({
+    //   emote: ['', Validators.required]
+    // });
   }
 
-  logForm() {
-    let headers = new Headers();
-    headers.append('Accept', 'application/json');
-    headers.append('Content-Type', 'application/json');
-    let options = new RequestOptions({ headers: headers });
+  // logForm() {
+  //   let headers = new Headers();
+  //   headers.append('Accept', 'application/json');
+  //   headers.append('Content-Type', 'application/json');
+  //   let options = new RequestOptions({ headers: headers });
 
-    let postParams = this.emoteForm.value;
+  //   let postParams = this.emoteForm.value;
 
-    this.http.post("https://whatsnext-api.herokuapp.com/emotes", postParams, options)
-    .map(res => res.json())
-    .subscribe(data => {
-      this.navCtrl.push('ResponsePage', {emote: data.action});
-      console.log(data);
-    }, error => {
-      console.log(error);
-    });
-  }
+  //   this.http.post("https://whatsnext-api.herokuapp.com/emotes", postParams, options)
+  //   .map(res => res.json())
+  //   .subscribe(data => {
+  //     this.navCtrl.push('ResponsePage', {emote: data.action});
+  //     console.log(data);
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  //}
 }
