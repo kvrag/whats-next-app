05e2913b4f35aaa258751c88730f3e292c33fd1c
Desmond Naranjo <dhnaranjo@gmail.com>
2017-07-29 20:20:10 -0500

    Visible form wo action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http'
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private emote: FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: Http, private formBuilder: FormBuilder) {
+    this.emote = this.formBuilder.group({
+      emote: ['', Validators.required]
+    });
+  }
 
+  logForm() {
+    console.log("Attempting logForm")
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+
+    let postParams = this.emote.value;
   }
 
 }
