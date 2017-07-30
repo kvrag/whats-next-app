1f4d4e743d81b2cde9160c4ebeedef07bb6aa600
Rachel <rachel.kerner@gmail.com>
2017-07-30 13:50:13 -0500

    Display response on new page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,37 @@
 import { Component } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http'
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms'
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+// import { ResponsePage } from '../response/response';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   private emoteForm: FormGroup;
 
-  constructor(public http: Http, private formBuilder: FormBuilder) {
+  constructor(public http: Http, public navCtrl: NavController, private formBuilder: FormBuilder) {
     this.emoteForm = this.formBuilder.group({
       emote: ['', Validators.required]
     });
   }
 
   logForm() {
-    console.log("Attempting logForm")
     let headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-Type', 'application/json');
     let options = new RequestOptions({ headers: headers });
 
     let postParams = this.emoteForm.value;
-    console.log(postParams)
 
     this.http.post("https://whatsnext-api.herokuapp.com/emotes", postParams, options)
+    .map(res => res.json())
     .subscribe(data => {
-      console.log(data['_body']);
+      this.navCtrl.push('ResponsePage', {emote: data.action});
+      console.log(data);
     }, error => {
       console.log(error);
     });
---
+++ src/pages/response/response.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-response',
+  templateUrl: 'response.html',
+})
+export class ResponsePage {
+  public emote: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.emote = this.navParams.get('emote');
+  }
+
+  ionViewDidLoad() {
+
+  }
+
+}
